fafcced00a62cec4e5ae84881e89ed149a0a3e0e
Saha <saha@Sahas-MacBook-Pro.fritz.box>
2026-06-30 09:47:00 +0200

    Fix production API URL to correct Render service name

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://mybraids-api.onrender.com/api',
+  apiUrl: 'https://mybraids.onrender.com/api',
   googleClientId: '938623436170-lv1p5d3i2uq87b7euu6bfduav9rlsf8g.apps.googleusercontent.com'
 };
